7d6cbca63475c2ac2dbf29996993aefaf237fa2e
Shreyas <vanamalashreyas@gmail.com>
2024-11-02 12:30:33 +0530

    commit

--- Affix tagger.py
+++ Affix tagger.py
@@ -1,9 +1,9 @@
 import nltk
 from nltk.corpus import treebank
 from nltk.tag import AffixTagger
-nltk.download('punkt_tab')
+#nltk.download('punkt_tab')
 
-sentence = "This is an example sentence."
+sentence = "This is an example sentence My name is shreyas."
 tokens = nltk.word_tokenize(sentence)
 train_data = treebank.tagged_sents()
 affix_tagger = AffixTagger(train_data, affix_length=-2)
