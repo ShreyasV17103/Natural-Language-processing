9392401fe91be93489b1ae9d05e39a82dcdf48ea
Shreyas <vanamalashreyas@gmail.com>
2024-11-02 12:33:21 +0530

    commit

--- Porter stemming.py
+++ Porter stemming.py
@@ -12,7 +12,7 @@ def tokenize_and_stem(input_text):
     return tokens, stemmed_tokens
 
 
-input_text = "This is a sample input string. It contains multiple words that will be tokenized and stemmed."
+input_text = " the Hindu festival of lights, with variations celebrated in other Indian religions such as Jainism and Sikhism.It symbolises the spiritual victory of Dharma over Adharma, light over darkness, good over evil, and knowledge over ignorance.Diwali is celebrated during the Hindu lunisolar months of Ashvin (according to the amanta tradition) and Kartika—between around mid-September and mid-November.The celebrations generally last five or six days"
 
 tokens, stemmed_tokens = tokenize_and_stem(input_text)
 print('Original Tokens:', tokens)
